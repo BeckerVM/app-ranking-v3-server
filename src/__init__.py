e99b500a9d11776611ee328fdfbbe04a9f869554
unknown <beiker2203@gmail.com>
2019-11-05 02:31:41 -0500

    Modificando a rutas absolutas

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-from .routes import test_router
+from src.routes import test_router
 
 app.register_blueprint(test_router.test_bp, url_prefix='/api/test')
